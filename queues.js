cf02c63ea51627914aae671294d6b6c9cb70c295
diurivj <diurivj@icloud.com>
2018-12-17 12:37:24 -0600

    queue good

---
+++ queues.js
@@ -0,0 +1,64 @@
+function createQueue() {
+	const queue = [] 
+	return {
+		enqueue(item) {
+			queue.unshift(item)
+		},
+		dequeue() {
+			return queue.pop()
+		},
+		peek() {
+			return queue[queue.length - 1]
+		},
+		get length() {
+			return queue.length
+		},
+		isEmpty() {
+			return queue.length === 0
+		}
+	}
+}
+
+function createPriorityQueue() {
+	const lowPriorityQueue = createQueue()
+	const highPriorityQueue = createQueue()
+
+	return {
+		enqueue(item, isHighPriority = false) {
+			isHighPriority
+			? highPriorityQueue.enqueue(item)
+			: lowPriorityQueue.enqueue(item)
+		},
+		dequeue() {
+			if (!highPriorityQueue.isEmpty()) {
+				return highPriorityQueue.dequeue()
+			}
+			return lowPriorityQueue.dequeue()
+		},
+		peek() {
+			if (!highPriorityQueue.isEmpty()) {
+				return highPriorityQueue.peek()
+			}
+			return lowPriorityQueue.peek()
+		},
+		length() {
+			return highPriorityQueue.length + 
+			lowPriorityQueue.length
+		},
+		isEmpty() {
+			return highPriorityQueue.isEmpty() &&
+			lowPriorityQueue.isEmpty()
+		}
+	}
+}
+
+const q = createPriorityQueue()
+
+q.enqueue('A fix here')
+q.enqueue('A bug there')
+q.enqueue('A new feature')
+
+q.dequeue()
+q.enqueue('Emergency task', true)
+q.dequeue()
+console.log(q.peek())
